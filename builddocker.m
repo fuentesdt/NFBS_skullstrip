6418b2f8e64f6fb5621ca8a557d7842fee9b2772
David Fuentes <fuentesdt@gmail.com>
2021-02-14 17:57:17 -0600

    WIP: @aecelaya

--- builddocker.m
+++ builddocker.m
@@ -1,6 +1,6 @@
 % https://www.mathworks.com/help/compiler/package-matlab-standalone-applications-into-docker-images.html
 res = compiler.build.standaloneApplication('brainSegmentation.m', 'TreatInputsAsNumeric', false)
-opts = compiler.package.DockerOptions(res,'ImageName','brainSegmentation-standalone-app')
+opts = compiler.package.DockerOptions(res,'ImageName','brainsegmentation-standalone-app')
 compiler.package.docker(res, 'Options', opts)
 % matlab -nodisplay -r "brainSegmentation('/rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/preprocess/imgResized/resized_T1w_A00060372.nii.gz', '/rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/fold_1-trainedDensenet3d.mat', '/rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/segout/seg_T1w_A00060372'); exit"
-% docker run --rm -e "DISPLAY=:0" -v /tmp/.X11-unix:/tmp/.X11-unix brainSegmentation-standalone-app 5 /rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/preprocess/imgResized/resized_T1w_A00060372.nii.gz /rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/fold_1-trainedDensenet3d.mat /rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/segout/seg_T1w_A00060372
+% docker run --rm -e "DISPLAY=:57" -v /tmp/.X11-unix:/tmp/.X11-unix brainsegmentation-standalone-app /rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/preprocess/imgResized/resized_T1w_A00060372.nii.gz /rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/fold_1-trainedDensenet3d.mat ./test
