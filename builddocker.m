a6e3e9aa3e55ef63c842eb95355014acfc398d0d
David Fuentes <fuentesdt@gmail.com>
2021-02-15 11:06:07 -0600

    DOC: usage

--- builddocker.m
+++ builddocker.m
@@ -2,6 +2,7 @@
 res = compiler.build.standaloneApplication('brainSegmentation.m', 'TreatInputsAsNumeric', false, 'AdditionalFiles','dicePixelClassification3dLayer.m')
 opts = compiler.package.DockerOptions(res,'ImageName','brainsegmentation-standalone-app')
 compiler.package.docker(res, 'Options', opts)
+% docker run  -it --rm  ubuntu:18.04
 % docker build -t brainsegmentation-standalone-app brainsegmentation-standalone-appdocker
 % matlab -nodisplay -r "brainSegmentation('/rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/preprocess/imgResized/resized_T1w_A00060372.nii.gz', '/rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/fold_1-trainedDensenet3d.mat', '/rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/segout/seg_T1w_A00060372'); exit"
 % brainSegmentationstandaloneApplication/run_brainSegmentation.sh /data/apps/MATLAB/R2020b /rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/preprocess/imgResized/resized_T1w_A00060372.nii.gz /rsrch1/ip/rmuthusivarajan/imaging/NFBS/Train_interpolation/fold_1-trainedDensenet3d.mat ./text
